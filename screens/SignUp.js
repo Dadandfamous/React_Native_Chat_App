af355e8a259b3b98e394c542a3608cfc17e94ba3
Martin <stuurmartinpost@gmail.com>
2019-06-12 17:34:53 +0200

    landingscreen functional

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,9 +1,7 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 
-import styles from './SignUp.styles'
-
-import React, { Component } from 'react'
+import styles from './SignUp.styles';
 
 export default class SignUp extends Component {
   render() {
@@ -11,8 +9,6 @@ export default class SignUp extends Component {
       <View style={styles.container}>
         <Text style={styles.title}>Sign up for Wird Word</Text>
       </View>
-    )
+    );
   }
 }
-
-export default SignUp
