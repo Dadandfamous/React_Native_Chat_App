9d139367a5549cef4544b14075f09177cb63b7fd
Martin <stuurmartinpost@gmail.com>
2019-06-12 17:28:50 +0200

    created basic signup page

---
+++ screens/SignUp.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+import styles from './SignUp.styles'
+
+import React, { Component } from 'react'
+
+export default class SignUp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Sign up for Wird Word</Text>
+      </View>
+    )
+  }
+}
+
+export default SignUp
